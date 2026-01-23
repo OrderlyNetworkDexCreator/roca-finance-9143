4ebdd127849689989fb786661c359a969c6322b0
Mario Reder <mario.reder@pm.me>
2026-01-23 20:25:44 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Portfolio,Markets,Leaderboard,Swap,Points,Trading",
-  "VITE_CUSTOM_MENUS": "CC BRIDGE, https://bridge.rocafi.com/; STAKE $ROCA, https://dapp.rocafi.com/; JOIN $ROCA PRESALE, https://ico.rocafi.com/register",
+  "VITE_CUSTOM_MENUS": "Stake $ROCA, https://dapp.rocafi.com/; Cross Chain Bridge, https://bridge.rocafi.com/; JOIN $ROCA PRESALE, https://ico.rocafi.com/register",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/rocafinance",
